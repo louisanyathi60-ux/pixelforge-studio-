8b1cb9521b42e54bfda75fa86223545147e89d32
Louisa Nyathi <louisanyathi60@gmail.com>
2026-02-12 21:32:40 +0200

    document.getElementById("contactForm").addEventListener("submit", function(e) {   e.preventDefault();   alert("Thank you! Your message has been received. We will contact you soon.");   this.reset(); });

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 document.getElementById("contactForm").addEventListener("submit", function(e) {
   e.preventDefault();
-
-  const name = e.target[0].value;
-  const email = e.target[1].value;
-  const message = e.target[2].value;
-
-  const mailtoLink = `mailto:your@email.com?subject=PixelForge Inquiry from ${name}&body=Email: ${email}%0D%0A%0D%0A${message}`;
-
-  window.location.href = mailtoLink;
+  alert("Thank you! Your message has been received. We will contact you soon.");
+  this.reset();
 });
